baa134fffc5cfc68499c96e466596ec97b256513
Carolina <carolinaruo@gmail.com>
2021-12-03 16:07:31 -0300

    adicionado funcao de remover item do cart, trabalhando no updateCartTotal

--- HomePage.js
+++ HomePage.js
@@ -1,5 +1,3 @@
-
-
 // let cartItem = document.getElementsByClass("addCart");
 
 // document.getElementsByClass("addCart").addEventListener("click", myFunction);
@@ -8,16 +6,30 @@
 //   document.getElementsByClass("addCart").createElement = ;
 // }
 
-(function(){
-    const addCart = document.querySelectorAll("addCart");
+// (function(){
+//     const addCart = document.querySelectorAll("addCart");
 
-    addCart.forEach(function(btn){
-        btn.addEventListener("click", function(e){
-            if (event.target.parentElement.classList.contains("addCart"))
-            {
-                console.log(event.target.parentElement.previousElementsSibling);
-            }
-        })
+//     addCart.forEach(function(btn){
+//         btn.addEventListener("click", function(e){
+//             if (event.target.parentElement.classList.contains("addCart"))
+//             {
+//                 console.log(event.target.parentElement.previousElementsSibling);
+//             }
+//         })
 
-    })
+//     })
+// })
+
+let removeCartItemButtons = document.getElementsByClassName("btn-danger");
+for (let i = 0; i < removeCartItemButtons.length; i++){
+    let button = removeCartItemButtons[i];
+    button.addEventListener("click", function(event) {
+    let buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove()
 })
+}
+
+function updateCartTotal() {
+    
+}
+
